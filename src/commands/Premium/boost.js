381030ea2254e8d1a203ac5be4940fde061f5184
ParasDeshpande <parasbpndeshpande@gmail.com>
2020-12-12 08:39:26 -0800

    removed filter too boost

--- src/commands/Premium/boost.js
+++ src/commands/Premium/boost.js
@@ -13,7 +13,6 @@ module.exports = class BoostCommand extends BaseCommand {
         if (!message.channel.clientPermissions.has("EMBED_LINKS")) return message.channel.send("I don't have permissions to send message embeds in this channel");
 
 
-        if (!message.author.permissions.discord.final.has("MANAGE_GUILD")) return message.channel.send(this.embedify(message.guild, "You need to have the `MANAGE_GUILD` permission on this server in order to use that command!", true));
         if (!await userData.premium.checkIfEnabled()) return message.channel.send(this.embedify(message.guild, `You need to have premium in order to use that command!\nVote on **[top.gg](${this.settings.client.info.externalSites["top.gg"]})** to get free premium for \`12 hours\`.`, true));
         if (await guildData.settings.premium.checkIfEnabled()) return message.channel.send(this.embedify(message.guild, `This guild already has premium enabled!`, true));
 
